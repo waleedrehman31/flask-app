04501b4238da1e81810c86e18bb7582965123ebb
yo-its-anas <anasejaz4042@gmail.com>
2026-03-16 13:05:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ r = redis.Redis(host=redis_host, port=redis_port)
 @app.route("/")
 def hello():
     count = r.incr("visits")
-    return f"Hello from Zynex Solutions! Jenkins CI/CD with CONTINOUS DEPLOYMENT!! Visits: {count}"
+    return f"Hello from Zynex Solutions! Jenkins CI/CD Completed!! Visits: {count}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
